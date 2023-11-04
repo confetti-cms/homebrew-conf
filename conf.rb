2224f0ef115c1eb793b7c946baaf4954a5110537
Reindert Vetter <reindertvetter+brew@gmail.com>
2023-11-04 13:30:51 +0100

    Brew formula update for conf version 0.7.2

--- conf.rb
+++ conf.rb
@@ -5,11 +5,11 @@
 class Conf < Formula
   desc ""
   homepage "https://confetti-cms.com"
-  version "0.7.1"
+  version "0.7.2"
 
   on_macos do
-    url "https://github.com/confetti-cms/conf/releases/download/0.7.1/conf_0.7.1_darwin_all.tar.gz"
-    sha256 "2f19d53f4cb89c7dddf2eb0ba187d1b277af3f4e713ad6f2560ad3f1670a0e1b"
+    url "https://github.com/confetti-cms/conf/releases/download/0.7.2/conf_0.7.2_darwin_all.tar.gz"
+    sha256 "0d46b7981516dcb9eb958bf72724b6c93e67129e87d4d3e8df78cf9135524826"
 
     def install
       bin.install "conf"
@@ -17,17 +17,17 @@ class Conf < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/confetti-cms/conf/releases/download/0.7.1/conf_0.7.1_linux_amd64.tar.gz"
-      sha256 "081541c6dd05f9dac8cd530e80f03b1ae81a345aa4fbb532935ffeb2d2ede662"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/confetti-cms/conf/releases/download/0.7.2/conf_0.7.2_linux_arm64.tar.gz"
+      sha256 "c556056b12e8b7e9fd140bd00f61ba9911597dd4a671cc969009a7cafdaf3a9d"
 
       def install
         bin.install "conf"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/confetti-cms/conf/releases/download/0.7.1/conf_0.7.1_linux_arm64.tar.gz"
-      sha256 "96ee9eea285a75a0efe9b793e2e81700d1a25d9f9375b47ada278622ae06c495"
+    if Hardware::CPU.intel?
+      url "https://github.com/confetti-cms/conf/releases/download/0.7.2/conf_0.7.2_linux_amd64.tar.gz"
+      sha256 "84d954ed7d332d6b30e328deb4337b73343be64808838b4b0cdd8165d60ae84d"
 
       def install
         bin.install "conf"
