b122ae949cdf428609096a33dee5fdedd51f1810
Reindert Vetter <reindertvetter+brew@gmail.com>
2026-03-04 21:43:37 +0100

    Brew formula update for conf version 1.42.0

--- conf.rb
+++ conf.rb
@@ -5,20 +5,20 @@
 class Conf < Formula
   desc ""
   homepage "https://confetti-cms.com"
-  version "1.41.0"
+  version "1.42.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/confetti-cms/conf/releases/download/1.41.0/conf_Darwin_x86_64.tar.gz"
-      sha256 "d67c1dcf448d30dda3e1d23070740222e12d1c913b68345f3db1a5a0ab9691c9"
+      url "https://github.com/confetti-cms/conf/releases/download/1.42.0/conf_Darwin_x86_64.tar.gz"
+      sha256 "0f9f83a0c85e0c6916ac8db3349a5c47d9d178314110a99c88570e08992255b0"
 
       def install
         bin.install "conf"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/confetti-cms/conf/releases/download/1.41.0/conf_Darwin_arm64.tar.gz"
-      sha256 "1d925c0f6762d85ae2ed4c49738b5b62958c6fd327fbb466acaec6484151d6ac"
+      url "https://github.com/confetti-cms/conf/releases/download/1.42.0/conf_Darwin_arm64.tar.gz"
+      sha256 "662099e0c8e3598404645fa113df60db41ee5ee74ed6680c19e6698f03ff2647"
 
       def install
         bin.install "conf"
@@ -29,8 +29,8 @@ class Conf < Formula
   on_linux do
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/confetti-cms/conf/releases/download/1.41.0/conf_Linux_x86_64.tar.gz"
-        sha256 "ae603c6dce146891f9432de8ae80edc8bee8a7235ecf553a30bd787711068dd9"
+        url "https://github.com/confetti-cms/conf/releases/download/1.42.0/conf_Linux_x86_64.tar.gz"
+        sha256 "2cb7cd8111d9c6a53bdb5d296246a8c71860170f5eda4154603f9d3737556d0b"
 
         def install
           bin.install "conf"
@@ -39,8 +39,8 @@ class Conf < Formula
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/confetti-cms/conf/releases/download/1.41.0/conf_Linux_arm64.tar.gz"
-        sha256 "5351c787cde563854e13cb3d43007506ab1cd2c2281f65a3d052aef200a2ff63"
+        url "https://github.com/confetti-cms/conf/releases/download/1.42.0/conf_Linux_arm64.tar.gz"
+        sha256 "60e31d7d702449e0ba942f660d095b8fd04b6e3d37d6fa0fb4ab7d794a375ae9"
 
         def install
           bin.install "conf"
